634496be4eb8caa549051f45f8a7d862f9fe399c
Jason Wu <jasonwu160160@gmail.com>
2024-09-04 21:18:12 -0700

    Update nacbag.lua

--- nacbag.lua
+++ nacbag.lua
@@ -90,69 +90,56 @@ end
 --local pval = 997
 --local sval = math.random(1,pval-1)
 --print("Secret Key",sval)
-local a, b = 401, 997
+local a, p = 401, 1097
 local pval = {2,3,5,7,11,13,17,19}
 local pubk = {0,0,0,0,0,0,0,0}
 
-local gcd, x, y = extended_gcd(a, b-1)
+local gcd, x, y = extended_gcd(a, p-1)
 --print("GCD:", gcd)
 print("Coefficients x and y:", x, y)
 for i = 1, #pval do
     --print("TEST,", modular_exponentiation(3,347,997))
-    pubk[i] = modular_exponentiation(pval[i],x,b)
+    pubk[i] = modular_exponentiation(pval[i],x,p)
 end
---local sroot, x, y = S_root(a, b-1)
-
+--local sroot, x, y = S_root(a, p-1)
 
 for i = 1 , #pubk do
     print("Public Key", pubk[i])
 end
 
-local mess = {11,40,50,61}
-binaryArray = {}
+local mess = {11,40,50,32}
+local binaryArray = {}
 
 for i = 1, #mess do
     binaryArray[i] = integerToBinaryArray(mess[i])
 end
 
---[[
-for i = 1, 8 do
-    print(binaryArray[i])
-end
-]]
-
 --Create Cipher
-cipher = {}
+local cipher = {}
 for i = 1,#mess do
     local c = 1
     for j = 1,8 do
-        c = (c * modular_exponentiation(pubk[j],binaryArray[i][j],b)) % b
+        c = (c * modular_exponentiation(pubk[j],binaryArray[i][j],p)) % p
         --print("Cipher - debug", c)
     end
     cipher[i] = c
 end
 
-for i = 1 ,#cipher do
+for i=1,#cipher do
     print(cipher[i])
 end
-
 --Decrypt Message
-
-val = 0
-
-for i = 1,4 do
-    print(modular_exponentiation(2,i,b))
-    val = val + ( (modular_exponentiation(2,i-1,b) * (invmod((pval[i]-1),b)) )) * ((itemgcd(pval[i],modular_exponentiation(895,401,b))-1))
-
-end
-
---( (pow(2,i,p)*invmod(pval[i]-1,p)) ) * (gcd(pval[i],pow(c,s,p))-1)
-
-print("ORIGINALMESS"..val%b)
---[[
-for i =1, #cipher do
-    for j =1, 8 do
-        
+local decipher = {}
+for i = 1,#mess do
+    local val = 0
+    for j = 1,8 do -- Summation (2^j-1 mod p * invmod p_i - 1) * (GCD(p_i,c^a mod p) - 1) 
+        val = (val + ( (modular_exponentiation(2,j-1,p) * (invmod((pval[j]-1),p)) )) * ((itemgcd(pval[j],modular_exponentiation(cipher[i],a,p))-1))) % p
     end
+
+    decipher[i] = val
+    --print("ORIGINALMESS"..val)
 end
-]]
+
+for i=1,#decipher do
+    print(decipher[i])
+end
